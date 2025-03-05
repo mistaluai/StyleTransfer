d722022f918ae26542d51f254116e061046dc8d4
Luai Waleed <mistaluai@gmail.com>
2025-03-05 09:31:35 +0200

    Update image_reconstruction.py

--- ImageReconstruction/image_reconstruction.py
+++ ImageReconstruction/image_reconstruction.py
@@ -1,7 +1,6 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
+from platform import processor
 
+import torch
 from ImageReconstruction.image_plotter import ImagePlotter
 from ImageReconstruction.image_processor import ImageProcessor
 from ImageReconstruction.image_reconstructor import ImageReconstructor
@@ -12,8 +11,18 @@ processor = ImageProcessor(device)
 target_image = processor.load_image('./images/ford.jpg')
 result_image = torch.rand(target_image.size(), requires_grad=True, device=device)
 
-reconstructor = ImageReconstructor(device)
+reconstructor = ImageReconstructor(device, is_local=True, reconstruction_layer='conv3_3')
 
-result_image, outputs = reconstructor.reconstruct(target_image, result_image, epochs=10)
+result_image, outputs = reconstructor.reconstruct(target_image, result_image, epochs=100)
+images = {
+'Generated Image': processor.tensor_to_image(result_image),
+'Ground Truth Image': processor.tensor_to_image(target_image)
+}
 
-plotter = ImagePlotter({'noise': processor.tensor_to_image(result_image), 'target_image': processor.tensor_to_image(target_image)})
+
+for i, image in enumerate(outputs):
+    if (i+1) % 10 == 0:
+        s = f'Generation step {i+1}'
+        images[s] = processor.tensor_to_image(image)
+
+plotter = ImagePlotter(images)
