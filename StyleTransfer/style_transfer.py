6b54b59b0f28f25046274bbe54f591610e6f6e47
Luai Waleed <mistaluai@gmail.com>
2025-03-08 23:57:19 +0200

    update main runner for the style transfer

--- StyleTransfer/style_transfer.py
+++ StyleTransfer/style_transfer.py
@@ -1,21 +1,20 @@
 import torch
 
 from StyleTransfer.style_transferer import StyleTransferer
-from utils.image_plotter import ImagePlotter
 from utils.image_processor import ImageProcessor
 
 device = 'cuda'
 processor = ImageProcessor(device)
 
-target_content = processor.load_image('./How-to-find-beautiful-landscapes-4.jpg')
-target_style = processor.load_image('./starry_night.jpg')
+target_style = processor.load_image('Villem_Ormisson,_Tartu_vaade_(1937).jpg')
+target_content = processor.load_image('Pyramid-of-Khafre-Giza-Egypt.jpg')
 
-# result = torch.rand(target_content.size(), requires_grad=True, device=device)
 result = target_content.clone().detach().contiguous().requires_grad_(True)
+result.add(torch.rand(result.size(), requires_grad=True, device=device))
 
 transferer = StyleTransferer(device=device, is_local=False)
 
-result, outputs = transferer.transfer(target_content, target_style, result, epochs=100, alpha=1, beta=1000000)
+result, outputs = transferer.transfer(target_content, target_style, result, epochs=100, alpha=10, beta=50)
 
 images = {
 'Generated Image': processor.tensor_to_image(result),
@@ -25,8 +24,6 @@ images = {
 
 
 for i, image in enumerate(outputs):
-    if (i+1) % 100 == 0:
+    if (i+1) % 50 == 0:
         s = f'Generation step {i+1}'
         images[s] = processor.tensor_to_image(image)
-name = 'StyleTransfer_StarryNightFordCar'
-plotter = ImagePlotter(data=images, gif_data=outputs, output_path='./st_outputs', name=name)
