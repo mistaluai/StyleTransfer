452cccf7b419384aac991fdd098ff96d37c7f8fd
Luai Waleed <mistaluai@gmail.com>
2025-03-08 08:04:15 +0200

    remove gram matrix normalization

--- utils/gram_matrix.py
+++ utils/gram_matrix.py
@@ -3,5 +3,4 @@ def gram_matrix(feature_maps):
     feature_maps = feature_maps.view(batch_size * num_channels, height * width)
     feature_maps_transposed = feature_maps.t()
     gram = feature_maps @ feature_maps_transposed
-    gram_n = gram / (batch_size * num_channels * height * width)
-    return gram_n
+    return gram
