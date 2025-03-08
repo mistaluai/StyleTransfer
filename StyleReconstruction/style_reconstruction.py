b85e34b16839f639c5c192ee234c6675441cd4d4
Luai Waleed <mistaluai@gmail.com>
2025-03-08 07:49:16 +0200

    adding style reconstruction test script

---
+++ StyleReconstruction/style_reconstruction.py
@@ -0,0 +1,29 @@
+import torch
+
+from StyleReconstruction.style_reconstructor import StyleReconstructor
+from utils.image_plotter import ImagePlotter
+from utils.image_processor import ImageProcessor
+from ImageReconstruction.image_reconstructor import ImageReconstructor
+
+device = 'mps'
+processor = ImageProcessor(device)
+
+target_style = processor.load_image('./images/starry_night.jpg')
+result_style = torch.rand(target_style.size(), requires_grad=True, device=device)
+
+reconstructor = StyleReconstructor(device=device, is_local=True)
+
+result_style, outputs = reconstructor.reconstruct(target_style, result_style, epochs=10)
+
+images = {
+'Generated Style': processor.tensor_to_image(result_style),
+'Ground Truth Style': processor.tensor_to_image(target_style)
+}
+
+
+for i, image in enumerate(outputs):
+    if (i+1) % 10 == 0:
+        s = f'Generation step {i+1}'
+        images[s] = processor.tensor_to_image(image)
+
+plotter = ImagePlotter(images)
