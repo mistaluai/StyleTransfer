2df55369963e6a4358df97cb5b7e10d8a20937c5
Luai Waleed <mistaluai@gmail.com>
2025-03-08 07:12:13 +0200

    implemented vgg19 model for style reconstruction

---
+++ StyleReconstruction/model.py
@@ -0,0 +1,18 @@
+import torch
+import torchvision.models as models
+
+from ImageReconstruction.model import ContentVGG19
+
+class StyleVGG19(ContentVGG19):
+    def __init__(self, device , reconstruction_layers, is_local=False):
+        super(ContentVGG19, self).__init__(device=device, is_local=is_local)
+        self.reconstruction_layers = [self.conv_layers[layer] for layer in reconstruction_layers]
+
+    def forward(self, x):
+        model = self.model # for readability
+        feature_maps = []
+        for i, layer in enumerate(model.children()):
+            x = layer(x)
+            if i in self.reconstruction_layers:
+                feature_maps.append(x)
+        return feature_maps
